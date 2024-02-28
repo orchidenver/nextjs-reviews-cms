8760f936c96ae56f1ea7041d79db6778444a6920
Vsevolod Slavianinov <a0974377355@gmail.com>
2024-02-28 18:24:44 +0200

    first commit

---
+++ lib/reviews.ts
@@ -0,0 +1,86 @@
+import { marked } from "marked";
+import qs from "qs";
+
+const CMS_URL = "http://localhost:1337";
+
+interface CmsItem {
+  id: number;
+  attributes: any;
+}
+
+export interface Review {
+  slug: string;
+  title: string;
+  date: string;
+  image: string;
+}
+
+export interface FullReview extends Review {
+  body: string;
+}
+
+export async function getFeaturedReview(): Promise<Review> {
+  const reviews = await getReviews();
+
+  return reviews[0];
+}
+
+export async function getReview(slug: string): Promise<FullReview> {
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ["slug", "title", "subtitle", "publishedAt", "body"],
+    populate: { image: { fields: ["url"] } },
+    pagination: { pageSize: 1, withCount: false },
+  });
+  const item = data[0];
+
+  return {
+    ...toReview(item),
+    body: marked(item.attributes.body),
+  };
+}
+
+export async function getReviews(): Promise<Review[]> {
+  const { data } = await fetchReviews({
+    fields: ["slug", "title", "subtitle", "publishedAt"],
+    populate: { image: { fields: ["url"] } },
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 6 },
+  });
+
+  return data.map(toReview);
+}
+
+export async function getSlugs(): Promise<string[]> {
+  const { data } = await fetchReviews({
+    fields: ["slug"],
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 100 },
+  });
+
+  return data.map((item: CmsItem) => item.attributes.slug);
+}
+
+async function fetchReviews(parameters: any) {
+  const url =
+    `${CMS_URL}/api/reviews?` +
+    qs.stringify(parameters, { encodeValuesOnly: true });
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
+  }
+
+  return await response.json();
+}
+
+function toReview(item: CmsItem): Review {
+  const { attributes } = item;
+
+  return {
+    slug: attributes.slug,
+    title: attributes.title,
+    date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
+    image: CMS_URL + attributes.image.data.attributes.url,
+  };
+}
---
+++ scripts/strapi-request.mjs
@@ -0,0 +1,16 @@
+import { writeFileSync } from 'node:fs';
+import qs from 'qs';
+
+const url = 'http://localhost:1337/api/reviews' + '?' + qs.stringify({
+    fields: ['slug', 'title', 'subtitle', 'publishedAt'], // add fields with data
+    populate: { image: { fields: ['url'] } }, // add media & path
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 6 }, // get 6 reviews initially
+}, { encodeValuesOnly: true }); // not encode params names, only values
+console.log('url:', url);
+const response = await fetch(url);
+const body = await response.json();
+const formatted = JSON.stringify(body, null, 2);
+const file = 'scripts/strapi-response.json'; // create a path and filename where the file with JSON data will be located
+writeFileSync(file, formatted, 'utf8'); // write response to a file
+
